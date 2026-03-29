18d20b56b1ab7503c1910c0c92da65c6cc8435b9
Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2026-03-29 23:28:10 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.get('/env', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-  res.json({ message: 'App is Ready'});
+  res.json({ message: 'App is Ready!'});
 });
 
 app.get('/api/users/:id', (_req, res) => {
