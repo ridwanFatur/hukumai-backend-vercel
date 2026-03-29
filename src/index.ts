34943c456bc47d81e24cd86c8a805ad4ef5b804f
Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2026-03-29 19:11:06 +0700

    Update

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
+});
+
+app.get('/api/users/:id', (_req, res) => {
+  res.json({ id: _req.params.id })
+})
+
+app.get('/api/posts/:postId/comments/:commentId', (_req, res) => {
+  res.json({ postId: _req.params.postId, commentId: _req.params.commentId })
+})
+
+export default app
