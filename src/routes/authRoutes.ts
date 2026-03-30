fd49ad70893a7a02d588efd538b2e85a4b2dfc73
Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2026-03-30 14:40:21 +0700

    Update

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 const router = Router();
 
+// TODO Login
+
 router.get('/google/config', (req, res) => {
-  res.json({ client_id: process.env.GOOGLE_CLIENT_ID });
+	res.json({ client_id: process.env.GOOGLE_CLIENT_ID });
 });
 
 export default router;
